f54da1994ae6e3492ce0ae2682510cbd32485edd
Gerison Kimathi Muriungi <gerkim62@gmail.com>
2025-11-22 18:48:48 +0300

    initial commit

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,16 @@
-import { PrismaClient } from './generated/prisma/client'
+import { PrismaClient } from './generated/prisma/client.js'
+
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
 
 declare global {
   var __prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.__prisma || new (PrismaClient as any)()
+export const prisma = globalThis.__prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.__prisma = prisma
