248fb4d0845c21b7ccde22d81b6c2e94b168cbfb
Gerison Kimathi Muriungi <gerkim62@gmail.com>
2025-11-23 16:56:15 +0300

    feat: update authentication configuration to require client IDs and secrets

--- src/env.ts
+++ src/env.ts
@@ -5,15 +5,14 @@ export const env = createEnv({
   server: {
     BETTER_AUTH_URL: z.url(),
     DATABASE_URL: z.string().min(1),
-    GITHUB_CLIENT_ID: z.string().optional(),
-    GITHUB_CLIENT_SECRET: z.string().optional(),
-    GOOGLE_CLIENT_ID: z.string().optional(),
-    GOOGLE_CLIENT_SECRET: z.string().optional(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
   clientPrefix: 'VITE_',
 
   client: {
-    VITE_BETTER_AUTH_URL: z.string().url(),
   },
 
   runtimeEnv: import.meta.env,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,27 +4,22 @@ import { tanstackStartCookies } from 'better-auth/tanstack-start'
 import { env } from '../env'
 import { prisma } from './prisma'
 
-const socialProviders: any = {}
 
-if (env.GITHUB_CLIENT_ID && env.GITHUB_CLIENT_SECRET) {
-  socialProviders.github = {
-    clientId: env.GITHUB_CLIENT_ID,
-    clientSecret: env.GITHUB_CLIENT_SECRET,
-  }
-}
-
-if (env.GOOGLE_CLIENT_ID && env.GOOGLE_CLIENT_SECRET) {
-  socialProviders.google = {
-    clientId: env.GOOGLE_CLIENT_ID,
-    clientSecret: env.GOOGLE_CLIENT_SECRET,
-  }
-}
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
   baseURL: env.BETTER_AUTH_URL,
-  socialProviders,
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
   plugins: [tanstackStartCookies()],
 })
