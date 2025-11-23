5ed487676c021a866e5d93125bc62499787886e2
Gerison Kimathi Muriungi <gerkim62@gmail.com>
2025-11-23 17:19:53 +0300

    feat: add owner relationship to Monitor model and update getMonitors function to filter by userId

--- src/features/monitor/get-monitors.ts
+++ src/features/monitor/get-monitors.ts
@@ -1,5 +1,37 @@
+import authMiddleware from '@/middlewares/auth-middleware'
 import { createServerFn } from '@tanstack/react-start'
+import { prisma } from '@/lib/prisma'
 
 const getMonitors = createServerFn({
   method: 'GET',
 })
+.middleware([authMiddleware]).handler(async ({ context }) => {
+  const { session:{session:{userId}} } = context
+
+  const monitors = await prisma.monitor.findMany({
+    where: {
+
+      ownerId: userId,
+    },
+    include:{
+      checks:{
+        orderBy:{
+          createdAt:'desc'
+        },
+        take:1,
+      }
+    }
+  })
+
+  const monitorsFormatted = monitors.map(monitor => ({
+    ...monitor,
+    lastCheck: monitor.checks[0] || null,
+  }))
+
+  return ({
+    monitors: monitorsFormatted,
+  })
+
+})
+
+export default getMonitors
